b42ad8818076972f3aacdff4e0d0ead0cbc7c328
Daniele Tabanella <daniele.tabanella@gmail.com>
2022-10-04 22:02:18 +0200

    Update

--- assets/Tiled/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="16" tileheight="16" tilecount="575" columns="25">
- <image source="Textures/tileset.png" width="400" height="368"/>
-</tileset>
